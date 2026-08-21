db5a55b0acdd25ff569850a7d944c0e2082b5dad
Malik Maarof <malikxp7@gmail.com>
2026-08-21 20:51:10 +0300

    Add Player Class

---
+++ Classes/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniRPG.Classes
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public float Gold { get; set; }
+        public int Level { get; set; }
+        public int XP { get; set; }
+
+        public Player( string name)
+        {
+            Name = name;
+            HP = 100;
+            MaxHP = 100;
+            Gold = 50;
+            Level = 1;
+            XP = 0;
+        }
+
+        public void printStats()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"HP: {HP}/{MaxHP}");
+            Console.WriteLine($"Gold: {Gold}");
+            Console.WriteLine($"Level: {Level}");
+            Console.WriteLine($"XP: {XP}");
+        }
+
+    }
+}
---
+++ MainClass.cs
@@ -0,0 +1,18 @@
+﻿using MiniRPG.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniRPG
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           Player player = new Player("Hero");
+            player.printStats();
+        }
+    }
+}
